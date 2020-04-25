236125e247b2477aea70567434a58ab145c7dc36
Richard Marko <srk@48.io>
2020-04-25 12:11:38 +0200

    Use <nixpkgs> and default haskellPackages

--- default.nix
+++ default.nix
@@ -1,13 +1,9 @@
-{ pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/8da81465c19fca393a3b17004c743e4d82a98e4f";
-    sha256 = "1f3s27nrssfk413pszjhbs70wpap43bbjx2pf4zq5x2c1kd72l6y";
-  }) {}
-}:
+{ pkgs ? import <nixpkgs> {} }:
 let
   inherit (pkgs) lib;
   hlib = pkgs.haskell.lib;
 
-  hpkgs = pkgs.haskell.packages.ghc865.extend (self: super: {
+  hpkgs = pkgs.haskellPackages.extend (self: super: {
     nixbot = (self.callCabal2nix "nixbot" (lib.sourceByRegex ./. [
       "^src.*$"
       "^.*\\.cabal$"
